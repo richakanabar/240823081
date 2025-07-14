578ec3d04caf29db01ec4eeccd5b3b5d54ceb79a
richakanabar <richakanabar14@gmail.com>
2025-07-14 11:17:32 +0530

    Practise set 1

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 const name = "Richa"
 console.log(name)
 
-const b=7
-if (b%2==0)
-{
-    console.log("number is even")
-}
-else
-{
-    console.log("number is odd")
-}
-consoile.log("Done..")
+
---
+++ practice_set1.js
@@ -0,0 +1,22 @@
+// const b=7
+// if (b%2==0)
+// {
+//     console.log("number is even")
+// }
+// else
+// {
+//     console.log("number is odd")
+// }
+// console.log("Done..")
+
+var num1=5
+var num2=10
+
+if (num1 >= num2)
+{
+    console.log("Num1 is bigger")
+}
+else
+{
+    console.log("Num2 is bigger")
+}
