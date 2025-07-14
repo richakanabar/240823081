7b7177d10aa9a9b3d040f28b206301aaea0e8aaf
richakanabar <richakanabar14@gmail.com>
2025-07-14 11:24:57 +0530

    prg3

--- practice_set1.js
+++ practice_set1.js
@@ -9,14 +9,26 @@
 // }
 // console.log("Done..")
 
-var num1=5
-var num2=10
+//2
+// var num1=5
+// var num2=10
 
-if (num1 >= num2)
+// if (num1 >= num2)
+// {
+//     console.log("Num1 is bigger")
+// }
+// else
+// {
+//     console.log("Num2 is bigger")
+// }
+
+//3
+var year=2020
+if (year%4==0)
 {
-    console.log("Num1 is bigger")
+    console.log("Leap Year")
 }
 else
 {
-    console.log("Num2 is bigger")
+    console.log("Not a leap year")
 }
