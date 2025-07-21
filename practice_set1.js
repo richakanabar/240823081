1aaf3e0ebe318fdf4763ff896dc34968dd6ed538
richakanabar <richakanabar14@gmail.com>
2025-07-21 11:37:24 +0530

    prgs

--- practice_set1.js
+++ practice_set1.js
@@ -65,23 +65,31 @@
 // let result = num1.toString().split('').reverse().join('');
 // console.log(result);
 
+// var num=1234
+// var rem,rev=0
+// while (num!=0){
+//     rem=num%10
+//     rev=rev*10+rem
+//     num=Math.floor(num/10)
+// }
+// console.log(rev)
+
 //8 palindrome
-// var num = 121;
-// var strNum = num.toString();
-// var isPalindrome = true; 
-
-// for (var i = 0; i < strNum.length / 2; i++) {
-//     if (strNum[i] !== strNum[strNum.length - 1 - i]) {
-//         isPalindrome = false;  
-//         break;  
-//     }
+// var num=121
+// var rem,rev=0
+// var sm=num
+// while (num!=0){
+//     rem=num%10
+//     rev=rev*10+rem
+//     num=Math.floor(num/10)
+// }
+// if(sm==rev){
+//     console.log("Palindrome:"+rev)
+// }
+// else{
+//     console.log("Not palindrome:"+rev)
 // }
 
-// if (isPalindrome) {
-//     console.log('Palindrome');
-// } else {
-//     console.log('Not Palindrome');
-// }
 
 
 //9 prime
@@ -107,19 +115,109 @@
 
 
 //10 count didgits in num
-let num = 12345;
-let count = 0;
+// let num = 12345
+// let count = 0
+// while (num!=0){
+//     num=Math.floor(num/10)
+//     count++
+// }
+// console.log(count)
 
-if (num === 0) {
-    count = 1; 
-} else {
-  
-    if (num < 0) {
-        num = -num;
-    }
-    while (num > 0) {
-        num = (num - (num % 10)) / 10;
-        count++;
-    }
-}
-console.log(count)
+//11 sum of digit
+// var sum=0,r
+// var no=123
+
+// while(no>0){
+//     r=no%10
+//     sum=sum+r
+//     no=Math.floor(no/10)
+// }
+// console.log(sum)
+
+//12 armstrong
+// var num = 153
+// var rem=0
+// var a=0
+// var r=num
+// var len=num.toString().length
+// while(num!=0){
+//     rem=num%10
+//     a=a+Math.pow(rem,len)
+//     num=Math.floor(num/10)
+// }
+// if(r==a){
+//     console.log("Armstring",a)
+// }
+// else{
+//     console.log("Not armstring",a)
+// }
+
+
+// 13. Generate Fibonacci Series
+// Definition: Generate the first n Fibonacci numbers.
+// Input: 5
+// Output: 0 1 1 2 3
+// var num=5
+// var a=0
+// var b=1
+// for(var i=0;i<=5;i++){
+//     console.log(a)
+//     c=a+b
+//     a=b
+//     b=c
+// }
+
+
+// 14. Check Vowel or Consonant
+// Definition: Determine if a given character is a vowel.
+// Input: a
+// Output: Vowel
+
+
+
+// 15. Simple Calculator
+// Definition: Perform +, -, *, / between two numbers.
+// Input: 4 + 2
+// Output: 6
+
+// 16. Find GCD (HCF)
+// Definition: Calculate the greatest common divisor of two numbers.
+// Input: 20 28
+// Output: 4
+
+// var num1=20
+// var num2=28
+// var div,num=0
+
+// if(num1<num2){
+//     num=num1
+// }
+// else{
+//     num=num2
+// }
+// for(var i=2;i<=num;i++){
+//     if(num1%i==0 && num2%i==0){
+//         div=i
+//     }
+// }
+// console.log(div)
+
+// 17. Check Perfect Number
+// Definition: A number whose sum of divisors equals itself.
+// Input: 28
+// Output: Perfect
+
+// 18. Print All Divisors
+// Definition: Display all positive divisors of a number.
+// Input: 10
+// Output: 1 2 5 10
+
+// 19. Number is Positive, Negative or Zero
+// Definition: Check if number is +ve, -ve, or zero.
+// Input: -5
+// Output: Negative
+
+// 20. Find Power (Exponentiation)
+// Definition: Compute a raised to the power b (a^b).
+// Input: 2 3
+// Output: 8
